2399db422e20961fed248889af1bd1e798db9727
Georgy Konstantinovich <mohammad.s.sbu91@gmail.com>
2026-05-25 12:14:59 +0330

    Fix undefined secure_token key on payment success view

--- app/Views/front/success.php
+++ app/Views/front/success.php
@@ -16,7 +16,12 @@
             <p class="text-muted mt-3">برای تکمیل سفارش، لطفاً مدارک خود را تکمیل کنید.</p>
             <p class="small text-muted">شماره تراکنش: <?= $ref_id ?></p>
             
-            <a href="<?= base_url('order/complete/' . $order['secure_token']) ?>" class="btn btn-primary mt-4">تکمیل اطلاعات و بارگذاری مدارک</a>
+            <?php $secureToken = $order['secure_token'] ?? ''; ?>
+            <?php if ($secureToken !== ''): ?>
+                <a href="<?= base_url('order/complete/' . $secureToken) ?>" class="btn btn-primary mt-4">تکمیل اطلاعات و بارگذاری مدارک</a>
+            <?php else: ?>
+                <a href="<?= current_url() ?>" class="btn btn-primary mt-4">تکمیل اطلاعات و بارگذاری مدارک</a>
+            <?php endif; ?>
         </div>
     </div>
 </div>
